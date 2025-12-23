871328d6bea6a690177e52eaf03c328ab9851ffb
PavaniKarunarathna <pavanikarunarathna21113@gmail.com>
2025-12-23 16:27:15 +0530

    graph.controller tests

---
+++ unit/backend/fixtures/kg.ts
@@ -0,0 +1,28 @@
+/**
+ Copyright 2025 JasmineGraph Team
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+export const runningKGMeta = {
+  id: 1,
+  user_id: 'user-1',
+  graph_id: 'graph-123',
+  hdfs_ip: '127.0.0.1',
+  hdfs_port: '9000',
+  hdfs_file_path: '/data/file',
+  llm_runner_string: 'runner:8080',
+  inference_engine: 'engine',
+  model: 'model',
+  chunk_size: 100,
+  status: 'running',
+  message: 'running',
+  cluster_id: 1,
+};
